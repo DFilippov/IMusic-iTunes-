8f500b083a1a73cb72448e2be20e31098856677c
Dmitri <porkcork@yandex.ru>
2019-10-06 16:56:25 +0300

    Initial Commit

---
+++ IMusic (iTunes)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IMusic (iTunes)
+//
+//  Created by Ayu Filippova on 06/10/2019.
+//  Copyright © 2019 Dmitry Filippov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
